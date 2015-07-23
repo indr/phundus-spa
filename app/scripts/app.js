b9195f34a29f3b05d07906298554f177026f37bd
Reto <reto@localhost.localdomain>
2015-07-23 08:41:33 +0200

    Generated app with yeoman.

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name phundusSpaApp
+ * @description
+ * # phundusSpaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('phundusSpaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phundusSpaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phundusSpaApp
+ */
+angular.module('phundusSpaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
