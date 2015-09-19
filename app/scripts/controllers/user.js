f4a0c63f3c4f98013a29ae0a62d364210b281956
indr <mail@indr.ch>
2015-09-19 08:04:38 +0200

    User home for visitors.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,12 +8,14 @@
  * Controller of the phundusApp
  */
 angular.module('phundusApp')
-  .controller('UserCtrl', ['$rootScope', '$scope', '$stateParams', 'Users',
-    function ($rootScope, $scope, $stateParams, Users) {
+  .controller('UserCtrl', ['$rootScope', '$scope', '$stateParams', 'Users', 'Auth',
+    function ($rootScope, $scope, $stateParams, Users, Auth) {
       $scope.loaded = false;
+      $scope.isHome = false;
 
       Users.get($stateParams.userId, function (res) {
         $scope.user = res;
+        $scope.isHome = $scope.user.userId === Auth.user.userId;
         $scope.loaded = true;
       }, function () {
         $rootScope.error = "Failed to fetch users.";
