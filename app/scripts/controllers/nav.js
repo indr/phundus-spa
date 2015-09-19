c406fe7dfceaeaea44755482858d3a40e4935f10
indr <mail@indr.ch>
2015-09-19 08:55:50 +0200

    Search box in navbar.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -8,11 +8,16 @@
  * Controller of the phundusApp
  */
 angular.module('phundusApp')
-  .controller('NavCtrl', ['$rootScope', '$scope', '$location', 'Auth',
-    function ($rootScope, $scope, $location, Auth) {
+  .controller('NavCtrl', ['$rootScope', '$scope', '$location', '$window', 'Auth',
+    function ($rootScope, $scope, $location, $window, Auth) {
       $scope.user = Auth.user;
       $scope.userRoles = Auth.userRoles;
       $scope.accessLevels = Auth.accessLevels;
+
+      $scope.queryString = '';
+      $scope.search = function () {
+        $window.location.href = '/shop/Search?queryString=' + $scope.queryString;
+      };
 
       $scope.select = function (membership) {
         Auth.select(membership, undefined, function (err) {
