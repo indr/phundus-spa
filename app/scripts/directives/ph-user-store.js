2a4c9ce144f9318be09900b42f464ce5c71c94fd
indr <mail@indr.ch>
2015-09-24 21:02:58 +0200

    Marker at coordinate.

--- app/scripts/directives/ph-user-store.js
+++ app/scripts/directives/ph-user-store.js
@@ -8,81 +8,96 @@
  * # phUserStore
  */
 angular.module('phundusApp')
-  .directive('phUserStore', ['$timeout', 'Stores', 'Alert', 'leafletData', function ($timeout, Stores, Alert, leafletData) {
-    return {
-      restrict: 'E',
-      replace: 'true',
-      scope: {
-        store: '=',
-        isEditable: '='
-      },
-      link: function (scope) {
+  .directive('phUserStore', ['$timeout', 'Stores', 'Alert', 'leafletData', 'leafletMarkersHelpers',
+    function ($timeout, Stores, Alert, leafletData, leafletMarkersHelpers) {
+      return {
+        restrict: 'E',
+        replace: 'true',
+        scope: {
+          store: '=',
+          isEditable: '='
+        },
+        link: function (scope) {
 
-        var flaechenschwerpunktDerSchweiz = {
-          lat: 46.80121,
-          lng: 8.226692
-        };
-        $timeout(function () {
-          leafletData.getMap().then(function (map) {
-            map.setView(flaechenschwerpunktDerSchweiz, 7);
+          var flaechenschwerpunktDerSchweiz = {
+            lat: 46.80121,
+            lng: 8.226692
+          };
+          
+          console.log(leafletData);
+
+          $timeout(function () {
+            leafletData.getMap().then(function (map) {
+              map.setView(flaechenschwerpunktDerSchweiz, 7);
+            });
+          }, 0);
+
+          var trySetViewToStore = function () {
+            if (scope.store && scope.store.coordinate) {
+              $timeout(function () {
+                leafletData.getMap().then(function (map) {
+                  var latLng = {
+                    lat: scope.store.coordinate.latitude,
+                    lng: scope.store.coordinate.longitude
+                  };
+                  map.setView(latLng, 16);
+
+                  if (!scope.marker) {
+                    scope.marker = leafletMarkersHelpers.createMarker(latLng, {
+                      draggable: true
+                    });
+                    scope.marker.addTo(map);
+                  }
+                  else {
+                    scope.marker.setLatLng(latLng);
+                  }
+                });
+              }, 500);
+            }
+          };
+
+          trySetViewToStore();
+
+          scope.fromMap = function () {
+            leafletData.getMap().then(function (map) {
+              var center = map.getCenter();
+              scope.userStoreCoordinateForm.latitude.$setViewValue(center.lat);
+              scope.userStoreCoordinateForm.latitude.$render();
+              scope.userStoreCoordinateForm.longitude.$setViewValue(center.lng);
+              scope.userStoreCoordinateForm.longitude.$render();
+            });
+          };
+
+          scope.$watch('store.coordinate.latitude', function () {
+            //console.log('watched: store.coordinate.latitude');
+            trySetViewToStore()
           });
-        }, 0);
+          scope.$watch('store.coordinate.longitude', function () {
+            //console.log('watched: store.coordinate.longitude');
+            trySetViewToStore()
+          });
 
-        var trySetViewToStore = function () {
-          if (scope.store && scope.store.coordinate) {
-            $timeout(function () {
-              leafletData.getMap().then(function (map) {
-                map.setView({
-                  lat: scope.store.coordinate.latitude,
-                  lng: scope.store.coordinate.longitude
-                }, 16);
-              });
-            }, 500);
+          scope.updateAddress = function ($data) {
+            Stores.putAddress(scope.store.storeId, $data.address, function () {
+            }, function (err) {
+              Alert.danger(err);
+            });
+          };
+          scope.updateOpeningHours = function ($data) {
+            Stores.putOpeningHours(scope.store.storeId, $data.openingHours, function () {
+            }, function (err) {
+              Alert.danger(err);
+            });
+          };
+
+          scope.updateCoordinate = function ($data) {
+            Stores.putCoordinate(scope.store.storeId, $data, function () {
+            }, function (err) {
+              Alert.danger(err);
+            });
           }
-        };
-
-        trySetViewToStore();
-
-        scope.fromMap = function () {
-          leafletData.getMap().then(function (map) {
-            var center = map.getCenter();
-            scope.userStoreCoordinateForm.latitude.$setViewValue(center.lat);
-            scope.userStoreCoordinateForm.latitude.$render();
-            scope.userStoreCoordinateForm.longitude.$setViewValue(center.lng);
-            scope.userStoreCoordinateForm.longitude.$render();
-          });
-        };
-
-        scope.$watch('store.coordinate.latitude', function () {
-          //console.log('watched: store.coordinate.latitude');
-          trySetViewToStore()
-        });
-        scope.$watch('store.coordinate.longitude', function () {
-          //console.log('watched: store.coordinate.longitude');
-          trySetViewToStore()
-        });
-
-        scope.updateAddress = function ($data) {
-          Stores.putAddress(scope.store.storeId, $data.address, function () {
-          }, function (err) {
-            Alert.danger(err);
-          });
-        };
-        scope.updateOpeningHours = function ($data) {
-          Stores.putOpeningHours(scope.store.storeId, $data.openingHours, function () {
-          }, function (err) {
-            Alert.danger(err);
-          });
-        };
-
-        scope.updateCoordinate = function ($data) {
-          Stores.putCoordinate(scope.store.storeId, $data, function () {
-          }, function (err) {
-            Alert.danger(err);
-          });
-        }
-      },
-      templateUrl: 'views/directives/ph-user-store.html'
-    }
-  }])
+        },
+        templateUrl: 'views/directives/ph-user-store.html'
+      }
+    }])
 ;
