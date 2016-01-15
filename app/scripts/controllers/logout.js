bdeec7c67dbe1b6dd9da0c3bcb7cd74ea5fe3a1d
indr <mail@indr.ch>
2016-01-15 06:03:31 +0100

    Log out error message.

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -8,15 +8,19 @@
  * Controller of the phundusApp
  */
 angular.module('phundusApp')
-  .controller('LogoutCtrl', ['$scope', '$location', '$window', 'Auth', 'Alert',
-    function ($scope, $location, $window, Auth, Alert) {
+  .controller('LogoutCtrl', ['$scope', '$location', '$window', 'Auth',
+    function ($scope, $location, $window, Auth) {
 
       if (Auth.isLoggedIn) {
         Auth.logout(function () {
             $location.path('/logged-out');
           },
           function (err) {
-            Alert.error(err);
+            var message = 'Der Server hat nicht geantwortet.';
+            if (err) {
+              message = err.message;
+            }
+            $scope.errorMessage = 'Fehler bei der Abmeldung: ' + message;
           });
       }
       else {
