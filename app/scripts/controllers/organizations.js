254ce8157d05df708a70360f0e544ac2c46f41f6
indr <mail@indr.ch>
2016-01-17 12:36:41 +0100

    Updated articles files url.

--- app/scripts/controllers/organizations.js
+++ app/scripts/controllers/organizations.js
@@ -411,7 +411,7 @@ angular.module('phundusApp')
 angular.module('phundusApp')
   .controller('OrganizationsArticlesFilesCtrl', ['$scope', 'organizationId', 'articleId',
     function ($scope, organizationId, articleId) {
-      $scope.url = '/api/v0/organizations/' + organizationId + '/articles/' + articleId + '/files';
+      $scope.url = '/api/v0/articles/' + articleId + '/files';
     }
   ]);
 
--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -240,7 +240,7 @@ angular.module('phundusApp')
 angular.module('phundusApp')
   .controller('UsersArticlesFilesCtrl', ['$scope', 'userId', 'articleId',
     function ($scope, userId, articleId) {
-      $scope.url = '/api/v0/users/' + userId + '/articles/' + articleId + '/files';
+      $scope.url = '/api/v0/articles/' + articleId + '/files';
     }
   ]);
 
