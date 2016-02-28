3f8648003a5dca1ea4af2c68e5dd878c0767d61a
indr <mail@indr.ch>
2016-02-28 13:55:08 +0800

    Change email address and change password as modal dialogs.

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -1,26 +1,33 @@
 'use strict';
 
 angular.module('phundusApp')
-  .controller('UsersAccountCtrl', ['$scope', 'Alert', 'AccountChangeEmailAddress', 'AccountChangePassword',
-    function ($scope, Alert, AccountChangeEmailAddress, AccountChangePassword) {
+  .controller('UsersAccountCtrl', ['$scope', '$uibModal',
+    function ($scope, $uibModal) {
 
-      $scope.submitChangeEmailAddress = function () {
-        $scope.formChangeEmailAddress.$submitting = true;
-        AccountChangeEmailAddress.post($scope.changeEmailAddress, function () {
-          $scope.formChangeEmailAddress.$submitting = false;
-          if ($scope.formChangeEmailAddress) {
-            $scope.formChangeEmailAddress.$setPristine();
-            $scope.formChangeEmailAddress.$setUntouched();
-          }
-          $scope.changeEmailAddress = {};
-          Alert.success('Das Bestätigungs-E-Mail wurde versendet.');
-        }, function () {
-          $scope.formChangeEmailAddress.$submitting = false;
-          Alert.error('Fehler beim Ändern der E-Mail-Addresse.')
+      $scope.showChangePassword = function () {
+        $uibModal.open({
+          templateUrl: 'views/account/modal-change-password.html',
+          controller: 'AccountChangePasswordCtrl'
         });
       };
 
-      $scope.submitChangePassword = function () {
+      $scope.showChangeEmailAddress = function () {
+        $uibModal.open({
+          templateUrl: 'views/account/modal-change-email-address.html',
+          controller: 'AccountChangeEmailAddressCtrl'
+        });
+      };
+    }
+  ]);
+
+angular.module('phundusApp')
+  .controller('AccountChangePasswordCtrl', ['$scope', '$uibModalInstance', 'Alert', 'AccountChangePassword',
+    function ($scope, $uibModalInstance, Alert, AccountChangePassword) {
+      $scope.ok = function () {
+        if (!$scope.formChangePassword.$valid) {
+          return;
+        }
+
         $scope.formChangePassword.$submitting = true;
         AccountChangePassword.post($scope.changePassword, function () {
           $scope.formChangePassword.$submitting = false;
@@ -29,12 +36,48 @@ angular.module('phundusApp')
             $scope.formChangePassword.$setUntouched();
           }
           $scope.changePassword = {};
+          $uibModalInstance.close();
           Alert.success('Das Passwort wurde geändert.');
         }, function () {
-          $scope.formChangePassword.$submitting = false;
+          $scope.formChangePassword.$suclosebmitting = false;
           Alert.error('Fehler beim Ändern des Passworts.')
         });
-      }
+      };
+
+      $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+      };
+    }
+  ]);
+
+angular.module('phundusApp')
+  .controller('AccountChangeEmailAddressCtrl', ['$scope', '$uibModalInstance', 'Alert', 'AccountChangeEmailAddress',
+    function ($scope, $uibModalInstance, Alert, AccountChangeEmailAddress) {
+      $scope.ok = function () {
+        if (!$scope.formChangeEmailAddress.$valid) {
+          return;
+        }
+
+        $scope.formChangeEmailAddress.$submitting = true;
+        AccountChangeEmailAddress.post($scope.changeEmailAddress, function () {
+          $scope.formChangeEmailAddress.$submitting = false;
+          if ($scope.formChangeEmailAddress) {
+            $scope.formChangeEmailAddress.$setPristine();
+            $scope.formChangeEmailAddress.$setUntouched();
+          }
+          $scope.changeEmailAddress = {};
+          $uibModalInstance.close();
+          Alert.success('Das Bestätigungs-E-Mail wurde versendet.');
+        }, function () {
+          $scope.formChangeEmailAddress.$submitting = false;
+          Alert.error('Fehler beim Ändern der E-Mail-Addresse.')
+        });
+
+      };
+
+      $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+      };
     }
   ]);
 
