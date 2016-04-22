4668b7a1f2635789f768e629725ca841746d1fa1
indr <mail@indr.ch>
2016-04-22 14:39:42 +0800

    Make link function explicit

--- app/modules/shop/directives/phShopItemAvailability.js
+++ app/modules/shop/directives/phShopItemAvailability.js
@@ -13,57 +13,58 @@
         itemId: '='
       },
       templateUrl: 'modules/shop/views/directives/phShopItemAvailability.html',
-      link: function (scope, element) {
+      link: link
+    };
 
-        ShopItemAvailability.get({itemId: scope.itemId}, function (res) {
-          scope.availabilities = res.result;
-          populateChartData(res.result);
+    function link(scope, element) {
+      ShopItemAvailability.get({itemId: scope.itemId}, function (res) {
+        scope.availabilities = res.result;
+        populateChartData(res.result);
 
-          // I'm totally not sure what I'm doing...
-          $compile(element.contents())(scope);
-        }, function (res) {
-          Alert.error('Fehler beim Laden der Verfügbarkeit: ' + res.data.message);
+        // I'm totally not sure what I'm doing...
+        $compile(element.contents())(scope);
+      }, function (res) {
+        Alert.error('Fehler beim Laden der Verfügbarkeit: ' + res.data.message);
+      });
+
+      function populateChartData(data) {
+        var labels = getLabels(data);
+        var values = getValues(data);
+
+        var chartData = {
+          labels: labels,
+          datasets: [
+            {
+              fillColor: "rgba(151,187,205,0.5)",
+              strokeColor: "rgba(151,187,205,1)",
+              pointColor: "rgba(151,187,205,1)",
+              pointStrokeColor: "#fff",
+              data: values
+            }
+          ]
+        };
+
+        scope.chart = {
+          data: chartData
+        };
+      }
+
+      function getLabels(data) {
+        var labels = _.map(data, function (each) {
+          return $filter('date')(each.fromUtc, 'mediumDate');
         });
+        labels.push('');
 
-        function populateChartData(data) {
-          var labels = getLabels(data);
-          var values = getValues(data);
+        return labels;
+      }
 
-          var chartData = {
-            labels: labels,
-            datasets: [
-              {
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                data: values
-              }
-            ]
-          };
+      function getValues(data) {
+        var values = _.map(data, function (each) {
+          return each.quantity;
+        });
+        values.push(values[values.length - 1]);
 
-          scope.chart = {
-            data: chartData
-          };
-        }
-
-        function getLabels(data) {
-          var labels = _.map(data, function (each) {
-            return $filter('date')(each.fromUtc, 'mediumDate');
-          });
-          labels.push('');
-
-          return labels;
-        }
-
-        function getValues(data) {
-          var values = _.map(data, function (each) {
-            return each.quantity;
-          });
-          values.push(values[values.length - 1]);
-
-          return values;
-        }
+        return values;
       }
     }
   }
